960d6f47dd3b27c04e5ea23703fece0ebb68233b
Rizwan Qureshi <rizwan@ualberta.ca>
2019-02-03 00:56:51 -0700

    added some matrix constructors, diagonal is left

--- smatrix.rb
+++ smatrix.rb
@@ -8,33 +8,89 @@ class SMatrix
 	include Test::Unit::Assertions
 
 	def initialize(rows, columns, *matrix)
+		# constructs a standard matrix
+
 		# pre
-		assert rows.is_a? Integer
-		assert columns.is_a? Integer
+		assert rows.is_a? Integer and columns.is_a? Integer
+		assert rows >= 0 and columns >= 0
 		
 		# post
 		@rows = rows
 		@columns = columns
-		@matrix = NMatrix[matrix]
+		@matrix = NMatrix[*matrix]
+
 		for i in 0 .. (rows - 1)
 			for j in 0 .. (columns - 1)
-				puts "#{i}#{j}"
+				# puts "#{i}#{j}"
 				assert_same(matrix[i][j], self[i, j])
 			end
 		end
+
 	end
 
 	def [](i, j)
 		# returns the matrix index at position i, j
 		
 		# pre
-		assert i.is_a? Integer
-		assert j.is_a? Integer
-		assert 0 <= i && i < @rows
-		assert 0 <= j && j < @columns
+		assert i.is_a? Integer and j.is_a? Integer
+		assert 0 <= i and i < @rows
+		assert 0 <= j and j < @columns
 
 		# post
 		return @matrix[i, j]
+	end
+
+	def []=(i, j, value)
+		# returns the matrix index at position i, j
+		
+		# pre
+		assert i.is_a? Integer and j.is_a? Integer
+		assert 0 <= i and i < @rows
+		assert 0 <= j and j < @columns
+
+		# post
+		@matrix[i, j] = value
+	end
+
+	def to_s
+		# converts the matrix into a string
+
+		# pre
+
+		# post
+		return @matrix.to_a.to_s
+	end
+
+	def identity?
+		# returns true if the matrix is an identity matrix
+
+		# pre
+		is_identity = true
+
+		# post
+		for i in 0 .. (@rows - 1)
+			for j in 0 .. (@columns - 1)
+				return false if (i == j and self[i, j] != 1) or (i != j and self[i, j] != 0)
+			end
+		end
+
+		return is_identity
+	end
+
+	def zero?
+		# returns true if the matrix is a zero matrix
+		
+		# pre
+		is_identity = true
+
+		# post
+		for i in 0 .. (@rows - 1)
+			for j in 0 .. (@columns - 1)
+				return false if self[i, j] != 0
+			end
+		end
+
+		return is_identity
 	end
 
 	@rows
@@ -43,18 +99,54 @@ class SMatrix
 
 end
 
-# class IdentityMatrix < SMatrix
+class IdentityMatrix < SMatrix
 
-# 	def initialize(rows, columns)
-# 		super(rows, columns)
-# 		assert @matrix.identity?
-# 	end
+	def initialize(size)
+		# constructs an identity matrix
 
-# end
+		# pre
+		assert size.is_a? Integer
+		@rows = @columns = size
+		@matrix = NMatrix.eye(size)
+
+		# post
+		assert self.identity?
+	end
+
+end
+
+class ZeroMatrix < SMatrix
+
+	def initialize(rows, columns)
+		# constructs a zero matrix
+
+		# pre
+		assert rows.is_a? Integer and columns.is_a? Integer
+		@rows = rows
+		@columns = columns
+		@matrix = NMatrix.zeros([rows, columns])
+
+		# post
+		assert self.zero?
+	end
+
+end
 
 s = SMatrix.new(3, 3,
 	[1, 2, 3],
 	[4, 5, 6],
 	[7, 8, 9]
 )
-puts s[0, 0]
+
+i = IdentityMatrix.new(3)
+
+z = ZeroMatrix.new(3, 3)
+
+print s.to_s
+puts
+
+print i.to_s
+puts
+
+print z.to_s
+puts
