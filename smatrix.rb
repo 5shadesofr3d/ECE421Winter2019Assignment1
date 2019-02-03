148c01ff7fc22e3153e62c53b4e5e8f45c90a577
Rizwan Qureshi <rizwan@ualberta.ca>
2019-02-03 01:07:31 -0700

    added some more invariants

--- smatrix.rb
+++ smatrix.rb
@@ -8,8 +8,10 @@ class SMatrix
 	include Test::Unit::Assertions
 
 	# --- Invariants ---
+	# @rows, @columns are Integers
 	# @rows >= 0
 	# @columns >= 0
+	#
 	# @matrix.is_a? NMatrix
 	# ------------------
 
@@ -72,7 +74,7 @@ class SMatrix
 		# returns true if the matrix is an identity matrix
 
 		# pre
-		is_identity = true
+		assert @columns == @rows
 
 		# post
 		for i in 0 .. (@rows - 1)
@@ -81,14 +83,13 @@ class SMatrix
 			end
 		end
 
-		return is_identity
+		return true
 	end
 
 	def zero?
 		# returns true if the matrix is a zero matrix
 		
 		# pre
-		is_identity = true
 
 		# post
 		for i in 0 .. (@rows - 1)
@@ -96,8 +97,6 @@ class SMatrix
 				return false if self[i, j] != 0
 			end
 		end
-
-		return is_identity
 	end
 
 	@rows
@@ -107,6 +106,10 @@ class SMatrix
 end
 
 class IdentityMatrix < SMatrix
+	# --- Invariants ---
+	# index [i, i] == 1
+	# index [i, j] == 0 if i =! j
+	# ------------------
 
 	def initialize(size)
 		# constructs an identity matrix
@@ -123,6 +126,9 @@ class IdentityMatrix < SMatrix
 end
 
 class ZeroMatrix < SMatrix
+	# --- Invariants ---
+	# index [i, j] == 0
+	# ------------------
 
 	def initialize(rows, columns)
 		# constructs a zero matrix
