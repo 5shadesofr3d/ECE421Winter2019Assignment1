e5afac6f825d1d76ac44c39fd8b7610d5d835735
joseram97 <jose@ualberta.ca>
2019-02-02 23:48:02 -0700

    added more data types to check

--- data-unitTestCases.rb
+++ data-unitTestCases.rb
@@ -11,60 +11,74 @@ class TestData < Test::Unit::TestCase
 
 	end
 	def testInt8
-		#test if the variable byte is held a true type
+		#test if the variable int is held a true type
 		matrix1 = NMatrix.new(3, 0, dtype: :int8)
 		assert_equal((matrix1.dtype.is_a? :int8.class), true)
 	end
 
 	def testInt16
-		#test if the variable byte is held a true type
+		#test if the variable int is held a true type
 		matrix1 = NMatrix.new(3, 0, dtype: :int8)
 		assert_equal((matrix1.dtype.is_a? :int8.class), true)
 	end
 
 	def testInt32
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the variable int is held a true type
+		matrix1 = NMatrix.new(3, 0, dtype: :int32)
+		assert_equal((matrix1.dtype.is_a? :int32.class), true)
 	end
 
 	def testInt64
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the variable int is held a true type
+		matrix1 = NMatrix.new(3, 0, dtype: :int64)
+		assert_equal((matrix1.dtype.is_a? :int64.class), true)
 	end
 
 	def testFloat32
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the floating type is valid for matrix creation
+		matrix1 = NMatrix.new(3, 0, dtype: :float32)
+		assert_equal((matrix1.dtype.is_a? :float32.class), true)
 	end
 
 	def testFloat64
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the floating type is valid for matrix creation
+		matrix1 = NMatrix.new(3, 0, dtype: :float64)
+		assert_equal((matrix1.dtype.is_a? :float64.class), true)
 	end
 
 	def testComplex64
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the complex type is held true for the matrix
+		matrix1 = NMatrix.new(3, 0, dtype: :complex64)
+		assert_equal((matrix1.dtype.is_a? :complex64.class), true)
 	end
 
 	def testComplex128
-		#test if the variable byte is held a true type
-		matrix1 = NMatrix.new(3, 0, dtype: :int8)
-		assert_equal((matrix1.dtype.is_a? :int8.class), true)
+		#test if the complex type is held true for the matrix
+		matrix1 = NMatrix.new(3, 0, dtype: :complex128)
+		assert_equal((matrix1.dtype.is_a? :complex128.class), true)
+	end
+
+	def testRubyObject
+		#check if the values being held are of the type object when created
+		matrix1 = NMatrix.new(3, 0, dtype: :object)
+		assert_equal((matrix1.dtype.is_a? :object.class), true)
+	end
+
+	def testRational32
+		#NOTE: this may be removed in future updates
+		matrix1 = NMatrix.new(3, 0, dtype: :rational32)
+		assert_equal((matrix1.dtype.is_a? :rational32.class), true)
+	end
+
+	def testRational64
+		#NOTE: this may be removed in future updates
+		matrix1 = NMatrix.new(3, 0, dtype: :rational64)
+		assert_equal((matrix1.dtype.is_a? :rational64.class), true)
+	end
+
+	def testRational128
+		#Check if the values in the matrix are rational type
+		matrix1 = NMatrix.new(3, 0, dtype: :rational128)
+		assert_equal((matrix1.dtype.is_a? :rational128.class), true)
 	end
 end
-
-# byte
-# int8
-# int16
-# int32
-# int64
-# float32
-# float64
-# complex64
-# complex128
