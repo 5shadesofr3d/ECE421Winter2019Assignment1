8affd37eb278520092f9d5c693c4b462befbcab9
Vishal Patel <vppatel@ualberta.ca>
2019-02-04 11:16:31 -0700

    Now matches rizwan's standard

--- SMatrix.rb
+++ SMatrix.rb
@@ -389,47 +389,55 @@ public
 
 	# TODO: Do these 2 functions violate dry?
 	def upper_triangle(k = 0)
+    assert valid?
 
 		# Pre-conditions
 		assert k.is_a? Integer and k >= 0
-		assert @matrix.shape.size == 2
+		assert @storage.shape.size == 2
 
 		# Post
-		@matrix.upper_triangle(k)
+		# @storage.upper_triangle(k)
 
+    assert valid?
 	end
 
 	def upper_triangle!(k = 0)
+    assert valid?
 
 		# Pre-conditions
 		assert k.is_a? Integer and k >= 0
-		assert @matrix.shape.size == 2
+		assert @storage.shape.size == 2
 
 		# Post
-		@matrix.upper_triangle!(k)
+    # @storage.upper_triangle!(k)
 
+    assert valid?
 	end
 
 	def lower_triangle(k = 0)
+    assert valid?
 
 		# Pre-conditions
 		assert k.is_a? Integer and k >= 0
-		assert @matrix.shape.size == 2
+		assert @storage.shape.size == 2
 
 		# Post
-		@matrix.lower_triangle(k)
+    # @storage.lower_triangle(k)
 
+    assert valid?
 	end
 
 	def lower_triangle!(k = 0)
+    assert valid?
 
 		# Pre-conditions
 		assert k.is_a? Integer and k >= 0
-		assert @matrix.shape.size == 2
+		assert @storage.shape.size == 2
 
 		# Post
-		@matrix.lower_triangle!(k)
+		# @storage.lower_triangle!(k)
 
+    assert valid?
 	end
 
 private
