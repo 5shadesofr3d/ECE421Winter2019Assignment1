845ffdb59b78cda8d0b7245312a6e2e5f0021875
Rizwan Qureshi <rizwan@ualberta.ca>
2019-02-03 21:09:25 -0700

    re indented code for tabs

--- SMatrix.rb
+++ SMatrix.rb
@@ -19,7 +19,6 @@ class SMatrix
 public
 	def initialize(*matrix)
 		# constructs a standard matrix
-
 		# pre
 
 		# post
@@ -96,7 +95,7 @@ public
 		# pre
 
 		# post
-		
+
 		@storage.each_index do |i, j|
 			false if self[i, j] != 0
 		end
@@ -113,135 +112,180 @@ public
 		Matrix[*@storage.to_a].diagonal?
 	end
 
+	#Generic add for all NMatrix types
 	def add(mat)
-	    #pre
-	    # mat = sparse yale format matrix
-	    # mat and _matrix must be matricies of the same size
+		#pre
+		assert mat.is_a? NMatrix
+		assert mat.shape == @matrix.shape
 
-	    #TODO: Main functionality
+		#TODO: Main functionality
+		result = @matrix + mat
+		@matrix = @matrix + mat
 
-	    @storage = @storage + mat
-
-	    #post
-	    # result = m1 + m2
-	    # result has same dimensions as m1 and m2
+		#post
+		assert @matrix.shape == mat.shape
+		assert result == @matrix
 	end
 
+	#Generic sub for all NMatrix types
 	def subtract(mat)
-	    #pre
-	    # mat = sparse yale format matrix
-	    # mat and _matrix must be matricies of the same size
+		#pre
+		assert mat.is_a? NMatrix
+		assert mat.shape == @matrix.shape
 
-	    #TODO: Main functionality
+		#TODO: Main functionality
+		result = @matrix - mat
+		@matrix = @matrix - mat
 
-	    @storage = @storage - mat
-
-
-	    #post
-	    # result = m1 - m2
+		#post
+		assert @matrix.shape == mat.shape
+		assert result == @matrix
 	end
 
+	#Generic divide for all NMatrix types
 	def divide(scalar)
-	    #pre
-	    # scalar = whole number integer to divide by
+		#pre
+		assert scalar.is_a? Integer
+		assert scalar != 0
 
-	    #TODO: Main functionality
+		#TODO: Main functionality
+		result = @matrix/scalar
+		@matrix = @matrix/scalar
 
-	    @storage = @storage/scalar
-
-	    #post
-	    # result = m1 / m2
+		#post
+		assert result == @matrix
 	end
 
+	#Generic exponent for all NMatrix types
 	def exponent(scalar)
-	    #pre
-	    # scalar = whole number integer to exponentiate by
+		#pre
+		assert scalar.is_a? Integer
 
-	    #TODO: Main functionality
+		#TODO: Main functionality
+		result = @matrix**scalar
+		@matrix = @matrix**scalar
 
-	    @storage = @storage**scalar
-
-
-	    #post
-	    # result = m1 ^ m2
+		#post
+		assert result == @matrix
 	end
 
+	#Generic multiply for all NMatrix types
 	def multiply(scalar)
-	    #pre
-	    # scalar = whole number integer to multiply by
+		#pre
+		assert scalar.is_a? Integer
 
-	    #TODO: Main functionality
-	    @storage = @storage*scalar
+		#TODO: Main functionality
+		result = @matrix*scalar
+		@matrix = @matrix*scalar
 
+		#post
+		assert result == @matrix
+	end
 
+	def dot(mat)
+		#pre
+		assert mat.is_a? NMatrix
 
-	    #post
-	    # result = m1 * m2
+		@matrix.dot(mat)
+
+		#post
+		#@matrix = @matrix DOT mat
+		assert mat.is_a? NMatrix
+	end
+
+	def trace()
+		#pre
+		#Matrix MUST be square to trace
+		assert @matrix.rows == @matrix.cols
+
+		@matrix.trace
+	end
+
+	def rank()
+		#TODO: Implement
+		#No real assertions here..?
+	end
+
+	def row_sum(rowNum)
+		assert rowNum.is_a? Integer
+		assert rowNum >= 0
+		assert rowNum < @matrix.rows
+		#TODO: Implement
+	end
+
+	def col_sum(colNum)
+		assert colNum.is_a? Integer
+		assert colNum >= 0
+		assert colNum < @matrix.cols
+		#TODO: Implement
+	end
+
+	def total_sum()
+		#TODO: Implement
+	end
+
+	def transpose()
+		#pre
+		#matrix to be transposed has been initialized
+		@matrix = @matrix.transpose()
+		#post
+		#@matrix = @matrix transposed
+	end
+
+	#Raise matrix to a power
+	def power(pow)
+		assert pow.is_a? Integer
+		assert @matrix.shape[0] == @matrix.shape[1] ##square matrix
+		@matrix.pow(pow)
+	end
+
+	def inverse()
+		#pre
+		assert @matrix.shape[0] == @matrix.shape[1] #square only
+
+		#TODO:
+		#NMatrix only implements this for dense matricies
+		#so we must convert types then calculate then convert back..
+		#inefficient but necessary for now
+
+		#post
+		# @matrix = @matrix^-1
+	end
+
+	def diagonal()
+		#pre
+
+		@matrix.diagonal()
+
+		#post
+	end
+
+	def determinant()
+		#pre
+		assert @matrix.shape[0] == @matrix.shape[1] #square
+		@matrix.det()
+		#post
+	end
+
+	def cholesky()
+		assert @matrix.symmetric? #Matrix MUST be symmetric
+		#TODO: Implement NMatrix cholesky factorization
+	end
+
+	def luDecomp
+		#pre
+
+		#TODO: Implement this, this has only been implemented for Dense
+		#matricies in NMatrix so conversions MUST be done
+
+		#post
+	end
+
+	def symmetric?
+		@matrix.symmetric?
 	end
 
 private
 	@storage
 
 end
-
-# class IdentityMatrix < SMatrix
-# 	# --- Invariants ---
-# 	# @rows == @columns
-# 	# index [i, i] == 1
-# 	# index [i, j] == 0 if i =! j
-# 	# ------------------
-
-# 	def initialize(size)
-# 		# constructs an identity matrix
-
-# 		# pre
-# 		assert size.is_a? Integer
-
-# 		# post
-# 		@rows = @columns = size
-# 		@storage = NMatrix.eye(size) # use factory here ?
-# 		assert self.identity?
-# 	end
-
-# end
-
-# class ZeroMatrix < SMatrix
-# 	# --- Invariants ---
-# 	# index [i, j] == 0
-# 	# ------------------
-
-# 	def initialize(rows, columns)
-# 		# constructs a zero matrix
-
-# 		# pre
-# 		assert rows.is_a? Integer and columns.is_a? Integer
-
-# 		# post
-# 		@rows = rows
-# 		@columns = columns
-# 		@storage = NMatrix.zeros([rows, columns]) # user factory here?
-# 		assert self.zero?
-# 	end
-
-# end
-
-# class DiagonalMatrix < SMatrix
-# 	# --- Invariants ---
-# 	# index [i, j] == 0 if i != j
-# 	# ------------------
-
-# 	def initialize(*values)
-# 		# constructs a diagonal matrix
-
-# 		# pre
-# 		for value in values
-# 			assert value.is_a? Numeric
-# 		end
-
-# 		# post
-# 		@rows = @columns = values.size
-# 		@storage = NMatrix.diagonal(values) # use factory here?
-# 		assert self.diagonal?
-# 	end
-
-# end
