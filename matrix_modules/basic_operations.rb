09e188e00ef57d004ad60e1b8b37058112f9c133
curtis goud <cgoud@ualberta.ca>
2019-02-04 19:55:53 -0700

    example module

---
+++ matrix_modules/basic_operations.rb
@@ -0,0 +1,33 @@
+require 'nmatrix'
+module NMatrixBasicOperations
+  def add(m1,m2)
+    assert m1.is_a? NMatrix and m2.is_a? NMatrix
+    result = m1+m2
+    return result
+  end
+
+  def subtract(m1,m2)
+    assert m1.is_a? NMatrix and m2.is_a? NMatrix
+    result = m1-m2
+    return result
+  end
+
+  def multiply(m1, scalar)
+    assert m1.is_a? NMatrix and scalar.is_a? Numeric
+    result = m1*scalar
+    return result
+  end
+
+  def divide(m1, scalar)
+    assert scalar != 0
+    assert m1.is_a? NMatrix and scalar.is_a? Numeric
+    result = m1/scalar
+    return result
+  end
+
+  def exponent(m1, scalar)
+    assert m1.is_a? NMatrix and scalar.is_a? Numeric
+    result = m1**scalar
+    return result
+  end
+end
