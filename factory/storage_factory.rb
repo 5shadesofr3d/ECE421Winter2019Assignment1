12c7eecde118139c540eb4d694c82baad6d84a13
Rizwan Qureshi <rizwan@ualberta.ca>
2019-02-08 20:44:23 -0700

    ok

--- factory/storage_factory.rb
+++ factory/storage_factory.rb
@@ -7,19 +7,44 @@ Yale = NMatrix
 
 class StorageFactory
 	include Test::Unit::Assertions
-	# --- Invariants ---
-	# @self.is_a? StorageFactory
-	# ------------------
+
+	TYPE = {
+		yale: Yale,
+		dok: Dok,
+		lil: Lil,
+		matrix: Matrix,
+		nmatrix: NMatrix
+	}
 
 	def create(matrix)
 		raise AbstractClassError
 	end
 
 	def self.valid?(storage)
-		return (storage.is_a? Yale or storage.is_a? Dok or storage.is_a? Lil or storage.is_a? Matrix or storage.is_a? Array)
+		return TYPE.has_value? storage.class
 	end
 
 	def valid?(storage)
 		return StorageFactory.valid? storage
+	end
+
+	def from_array(storage)
+		# returns a Dok object which contains content from storage
+
+		# pre
+		assert storage.is_a? Array
+
+		# post
+		rows = storage.size
+		columns = storage[0].size if rows > 0
+		dok = Dok.new(rows, columns)
+
+		for i in 0 .. rows - 1
+			for j in 0 .. columns - 1
+				dok[i, j] = storage[i][j]
+			end
+		end
+
+		dok
 	end
 end
