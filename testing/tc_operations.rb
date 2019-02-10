04f30af1012383ea53e7379e41b4ec47b6a4df96
Jori Romans <jromans@ualberta.ca>
2019-02-09 21:03:51 -0700

    Finished multiplcation test

--- testing/tc_operations.rb
+++ testing/tc_operations.rb
@@ -156,18 +156,38 @@ class TestOperations<Test::Unit::TestCase
 
   #TODO: Improve this
   def test_multiplication
-    #Init matrix1 as 3x3 matrix of all 0's
-    matrix1 = Matrix.new(3)
+    sMatrix1 = SMatrix.new(Yale.new(3, 3))
+    sMatrix2 = SMatrix.new(Dok.new(3, 3), :dok)
+    sMatrix3 = SMatrix.new(Lil.new(3, 3), :lil)
 
-    #Init matrix2 as 3x3 matrix of all
-    ##Pre conditions
+    sMatrixProduct = SMatrix.new(Yale.new(3, 3))
 
-    #perform operation
-    scalar = 2
-    matrix1.multiply(scalar)
+    value = 1
 
-    ##Post conditions
-    assert_equal(NMatrix.new(3),matrix1)
+    for i in 0..2 do
+      for j in 0..2 do
+        sMatrix1[i, j] = value
+        sMatrix2[i, j] = value
+        sMatrix3[i, j] = value
+
+        sMatrixProduct[i, j] = value * 3
+        value += 1
+      end
+    end
+
+    #assert sMatrix3 == sMatrix1 * 2
+    sMatrix4 = sMatrix1 * 3 #should be of type Yale
+    sMatrix5 = sMatrix2 * 3#should be of type DoK
+    sMatrix6 = sMatrix3 * 3#should be of type Lil
+
+    assert(sMatrixProduct.equals(sMatrix4)) #yale addition works
+    assert(sMatrix4.type == Yale) #still a yale
+
+    assert(sMatrixProduct.equals(sMatrix5)) #Dok addition works
+    assert(sMatrix5.type == Dok) #still a Dok
+
+    assert(sMatrixProduct.equals(sMatrix6)) #Lil addition works
+    assert(sMatrix6.type == Lil) #still a Lil
   end
 
 end
