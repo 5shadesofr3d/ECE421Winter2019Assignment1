02a3ddb10515c8b01e9ea1eb5ad83dfc03fabdfd
Jori Romans <jromans@ualberta.ca>
2019-02-09 20:48:34 -0700

    Reorganized test cases to seperate folder

--- testing/tc_operations.rb
+++ testing/tc_operations.rb
@@ -1,8 +1,8 @@
-require './SMatrix'
+require_relative '../SMatrix'
 require 'test/unit'
-require_relative 'storage/yale'
-require_relative 'storage/dok'
-require_relative 'storage/lil'
+require_relative '../storage/yale'
+require_relative '../storage/dok'
+require_relative '../storage/lil'
 require 'matrix'
 
 class TestOperations<Test::Unit::TestCase
