40c3b067c997a9d237f56df7af4c4d5ca3554f0c
Jori Romans <jromans@ualberta.ca>
2019-02-10 21:11:00 -0700

    test_identity complete

--- testing/tc_conditionals.rb
+++ testing/tc_conditionals.rb
@@ -13,6 +13,55 @@ class TestConditionals<Test::Unit::TestCase
 	# probably just use extreme point analysis for the data points, etc.
 
 	def test_identity
+		#setup
+
+		yaleMatrix = SMatrix.new(Yale.new(3, 3))
+		dokMatrix = SMatrix.new(Dok.new(3, 3), :dok)
+		lilMatrix = SMatrix.new(Lil.new(3, 3), :lil)
+
+		for i in 0..2 do
+			for j in 0..2 do
+				if i == j
+					value = 1
+				else
+					value = 0
+				end
+
+				yaleMatrix[i, j] = value
+				dokMatrix[i, j] = value
+				lilMatrix[i, j] = value
+
+			end
+		end
+
+		#Ensure all equal the 3x3 identity matrix
+
+		assert(yaleMatrix.identity?)
+		assert(dokMatrix.identity?)
+		assert(lilMatrix.identity?)
+
+		#Take away a different 1 from each
+
+		yaleMatrix[0,0] = 0
+		dokMatrix[1,1] = 0
+		lilMatrix[2,2] = 0
+
+		assert(!yaleMatrix.identity?)
+		assert(!dokMatrix.identity?)
+		assert(!lilMatrix.identity?)
+
+		#Add one back, and an extra 1 elsewhere
+
+		yaleMatrix[0,0] = 1
+		yaleMatrix[0,1] = 1
+		dokMatrix[1,1] = 1
+		dokMatrix[1,2] = 1
+		lilMatrix[2,2] = 1
+		lilMatrix[2,0] = 1
+
+		assert(!yaleMatrix.identity?)
+		assert(!dokMatrix.identity?)
+		assert(!lilMatrix.identity?)
 
 	end
 
