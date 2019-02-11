4aaf6e801bebcac89edc38a4f319f5d413d094fb
joseram97 <jose@ualberta.ca>
2019-02-10 20:05:33 -0700

    Starting out with the builder tests

--- testing/tc_builder.rb
+++ testing/tc_builder.rb
@@ -5,8 +5,17 @@ require_relative '../storage/dok'
 require_relative '../storage/lil'
 require 'matrix'
 
+STest = SMatrix
+
 class TestBuilder<Test::Unit::TestCase
 	# this is to test all of the functions in the builder module for smatrix
+	@@maxsize = 100
+
+	def setup
+		# start setting up the variables for the test cases
+		@row = @col = (rand * @@maxsize).to_i
+		#@eye = STest.
+	end
 
 	def test_I
 		# This test will cover both the "I" and the "Eye" functions in the module
