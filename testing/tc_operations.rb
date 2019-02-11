d93309a9e41a4e8699ab04731824d15584b7ed10
Jori Romans <jromans@ualberta.ca>
2019-02-11 13:29:33 -0700

    More test cases for operations

--- testing/tc_operations.rb
+++ testing/tc_operations.rb
@@ -173,85 +173,171 @@ class TestOperations<Test::Unit::TestCase
 
   end
 
-  #def test_determinant
-    #yaleMatrix= SMatrix.new(Yale.new(2, 2))
-    #dokMatrix = SMatrix.new(Dok.new(2, 2), :dok)
-    #lilMatrix = SMatrix.new(Lil.new(2, 2), :lil)
+  def test_determinant
+    yaleMatrix= SMatrix.new(Yale.new(2, 2))
+    dokMatrix = SMatrix.new(Dok.new(2, 2), :dok)
+    lilMatrix = SMatrix.new(Lil.new(2, 2), :lil)
 
-    #for i in 0..1 do
-      #for j in 0..1 do
-        #value = rand(10)
+    for i in 0..1 do
+      for j in 0..1 do
+        value = rand(10)
 
-        #yaleMatrix[i, j] = value
-        #dokMatrix[i, j] = value
-        #lilMatrix[i, j] = value
+        yaleMatrix[i, j] = value
+        dokMatrix[i, j] = value
+        lilMatrix[i, j] = value
 
-      #end
-    #end
+      end
+    end
 
-    #det = (yaleMatrix[0, 0] * yaleMatrix[1, 1]) - (yaleMatrix[0, 1] * yaleMatrix[1, 0])
+    det = (yaleMatrix[0, 0] * yaleMatrix[1, 1]) - (yaleMatrix[0, 1] * yaleMatrix[1, 0])
 
-    #yaleInverse = yaleMatrix.determinant
-    #dokInverse = dokMatrix.determinant
-    #lilInverse = lilMatrix.determinant
-  #end
+    yaleInverse = yaleMatrix.determinant
+    dokInverse = dokMatrix.determinant
+    lilInverse = lilMatrix.determinant
+  end
 
   def test_cholesky
     yaleMatrix = SMatrix.new(Yale.new(3, 3))
     dokMatrix = SMatrix.new(Dok.new(3, 3), :dok)
     lilMatrix = SMatrix.new(Lil.new(3, 3), :lil)
-    resultMatrix = SMatrix.new(Yale.new(3, 3))
+    resultMatrixOne = SMatrix.new(Yale.new(3, 3))
+    resultMatrixTwo = SMatrix.new(Yale.new(3, 3))
 
-    yaleMatrix[0, 0] = 25
-    dokMatrix[0, 0] = 25
-    lilMatrix[0, 0] = 25
-    resultMatrix[0, 0] = 5
+    yaleMatrix[0, 0] = 4
+    dokMatrix[0, 0] = 4
+    lilMatrix[0, 0] = 4
+    resultMatrixOne[0, 0] = 2
+    resultMatrixTwo[0, 0] = 2
 
-    yaleMatrix[0, 1] = 15
-    dokMatrix[0, 1] = 15
-    lilMatrix[0, 1] = 15
-    resultMatrix[0, 1] = 0
+    yaleMatrix[0, 1] = 12
+    dokMatrix[0, 1] = 12
+    lilMatrix[0, 1] = 12
+    resultMatrixOne[0, 0] = 0
+    resultMatrixTwo[0, 0] = 6
 
-    yaleMatrix[0, 2] = -5
-    dokMatrix[0, 2] = -5
-    lilMatrix[0, 2] = -5
-    resultMatrix[0, 2] = 0
+    yaleMatrix[0, 2] = -16
+    dokMatrix[0, 2] = -16
+    lilMatrix[0, 2] = -16
+    resultMatrixOne[0, 0] = 0
+    resultMatrixTwo[0, 0] = -8
 
-    yaleMatrix[1, 0] = 15
-    dokMatrix[1, 0] = 15
-    lilMatrix[1, 0] = 15
-    resultMatrix[1, 0] = 3
+    yaleMatrix[1, 0] = 12
+    dokMatrix[1, 0] = 12
+    lilMatrix[1, 0] = 12
+    resultMatrixOne[0, 0] = 6
+    resultMatrixTwo[0, 0] = 0
 
-    yaleMatrix[1, 1] = 18
-    dokMatrix[1, 1] = 18
-    lilMatrix[1, 1] = 18
-    resultMatrix[1, 1] = 3
+    yaleMatrix[1, 1] = 37
+    dokMatrix[1, 1] = 37
+    lilMatrix[1, 1] = 37
+    resultMatrixOne[0, 0] = 1
+    resultMatrixTwo[0, 0] = 1
 
-    yaleMatrix[1, 2] = 0
-    dokMatrix[1, 2] = 0
-    lilMatrix[1, 2] = 0
-    resultMatrix[1, 2] = 0
+    yaleMatrix[1, 2] = -43
+    dokMatrix[1, 2] = -43
+    lilMatrix[1, 2] = -43
+    resultMatrixOne[0, 0] = 0
+    resultMatrixTwo[0, 0] = 5
 
-    yaleMatrix[2, 0] = -5
-    dokMatrix[2, 0] = -5
-    lilMatrix[2, 0] = -5
-    resultMatrix[2, 0] = -1
+    yaleMatrix[2, 0] = -16
+    dokMatrix[2, 0] = -16
+    lilMatrix[2, 0] = -16
+    resultMatrixOne[0, 0] = -8
+    resultMatrixTwo[0, 0] = 0
 
-    yaleMatrix[2, 1] = 0
-    dokMatrix[2, 1] = 0
-    lilMatrix[2, 1] = 0
-    resultMatrix[2, 1] = 1
+    yaleMatrix[2, 1] = -43
+    dokMatrix[2, 1] = -43
+    lilMatrix[2, 1] = -43
+    resultMatrixOne[0, 0] = 5
+    resultMatrixTwo[0, 0] = 0
 
-    yaleMatrix[2, 2] = 11
-    dokMatrix[2, 2] = 11
-    lilMatrix[2, 2] = 11
-    resultMatrix[2, 2] = 3
+    yaleMatrix[2, 2] = 98
+    dokMatrix[2, 2] = 98
+    lilMatrix[2, 2] = 98
+    resultMatrixOne[0, 0] = 3
+    resultMatrixTwo[0, 0] = 3
 
-    assert(resultMatrix.equals(yaleMatrix.cholesky))
-    assert(resultMatrix.equals(dokMatrix.cholesky))
-    assert(resultMatrix.equals(lilMatrix.cholesky))
+    assert(resultMatrixOne.equals(yaleMatrix.cholesky[0]))
+    assert(resultMatrixTwo.equals(yaleMatrix.cholesky[1]))
+    assert(resultMatrixOne.equals(dokMatrix.cholesky[0]))
+    assert(resultMatrixTwo.equals(dokMatrix.cholesky[1]))
+    assert(resultMatrixOne.equals(lilMatrix.cholesky[0]))
+    assert(resultMatrixTwo.equals(lilMatrix.cholesky[1]))
 
   end
 
+  def test_lu
+    yaleMatrix = SMatrix.new(Yale.new(3, 3))
+    dokMatrix = SMatrix.new(Dok.new(3, 3), :dok)
+    lilMatrix = SMatrix.new(Lil.new(3, 3), :lil)
+    resultMatrixOne = SMatrix.new(Yale.new(3, 3))
+    resultMatrixTwo = SMatrix.new(Yale.new(3, 3))
 
+    yaleMatrix[0, 0] = 1
+    dokMatrix[0, 0] = 1
+    lilMatrix[0, 0] = 1
+    resultMatrixOne[0, 0] = 1
+    resultMatrixTwo[0, 0] = 1
+
+    yaleMatrix[0, 1] = 2
+    dokMatrix[0, 1] = 2
+    lilMatrix[0, 1] = 2
+    resultMatrixOne[0, 1] = 0
+    resultMatrixTwo[0, 1] = 2
+
+    yaleMatrix[0, 2] = 4
+    dokMatrix[0, 2] = 4
+    lilMatrix[0, 2] = 4
+    resultMatrixOne[0, 2] = 2
+    resultMatrixTwo[0, 2] = 4
+
+    yaleMatrix[1, 0] = 3
+    dokMatrix[1, 0] = 3
+    lilMatrix[1, 0] = 3
+    resultMatrixOne[1, 0] = 3
+    resultMatrixTwo[1, 0] = 0
+
+    yaleMatrix[1, 1] = 8
+    dokMatrix[1, 1] = 8
+    lilMatrix[1, 1] = 8
+    resultMatrixOne[1, 1] = 1
+    resultMatrixTwo[1, 1] = 2
+
+    yaleMatrix[1, 2] = 14
+    dokMatrix[1, 2] = 14
+    lilMatrix[1, 2] = 14
+    resultMatrixOne[1, 2] = 0
+    resultMatrixTwo[1, 2] = 2
+
+    yaleMatrix[2, 0] = 2
+    dokMatrix[2, 0] = 2
+    lilMatrix[2, 0] = 2
+    resultMatrixOne[2, 0] = 2
+    resultMatrixTwo[2, 0] = 0
+
+    yaleMatrix[2, 1] = 6
+    dokMatrix[2, 1] = 6
+    lilMatrix[2, 1] = 6
+    resultMatrixOne[2, 1] = 1
+    resultMatrixTwo[2, 1] = 0
+
+    yaleMatrix[2, 2] = 13
+    dokMatrix[2, 2] = 13
+    lilMatrix[2, 2] = 13
+    resultMatrixOne[2, 2] = 1
+    resultMatrixTwo[2, 2] = 3
+
+    assert(yaleMatrix.lu_factorization[0].equals(resultMatrixOne))
+    assert(yaleMatrix.lu_factorization[1].equals(resultMatrixTwo))
+    assert(dokMatrix.lu_factorization[0].equals(resultMatrixOne))
+    assert(dokMatrix.lu_factorization[1].equals(resultMatrixTwo))
+    assert(lilMatrix.lu_factorization[0].equals(resultMatrixOne))
+    assert(lileMatrix.lu_factorization[1].equals(resultMatrixTwo))
+
+
+  end
+
+  def test_conjugate
+
+  end
 end
