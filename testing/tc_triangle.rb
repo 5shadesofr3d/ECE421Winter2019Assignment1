d4fec634ce6f49f0b0936b8614da5dd34f9011f4
joseram97 <jose@ualberta.ca>
2019-02-11 12:52:57 -0700

    Modified the tc_conversions.rb file

--- testing/tc_triangle.rb
+++ testing/tc_triangle.rb
@@ -8,12 +8,15 @@ require 'matrix'
 STest = SMatrix
 
 class TestConversions<Test::Unit::TestCase
-	# this is to test all of the functions in the builder module for smatrix
-	@@maxsize = 100
+	# this is to test all of the functions in the triangle module for SMatrix
+	# This will be releasing boolean variables for checking that a matrix is of upper, or
+	# lower triangle
+	@@maxsize = 50
 
 	def setup
 		# start setting up the variables for the test cases
 		# NOTE: some of these "to_i" must be confirmed in another test
+		@row = @col = (rand * @@maxsize).to_i
 	end
 
 	def test_upper_triangle
